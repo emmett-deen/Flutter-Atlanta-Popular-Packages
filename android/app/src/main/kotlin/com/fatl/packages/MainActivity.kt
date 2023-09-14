adad42d1299b1f3f0aede5a86d13aaf0764c284e
Emmett Deen <emmett.deen@kickasscoding.com>
2023-09-14 09:46:08 -0400

    feat: project init

---
+++ android/app/src/main/kotlin/com/fatl/packages/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fatl.packages.flutter_atlanta_packages_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_atlanta_packages_example/app/app.dart';
+import 'package:flutter_atlanta_packages_example/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
